193874c397a074a7fd9f2db883ce4282151ef077
Mark Tovar <markjtovar24@gmail.com>
2024-05-09 22:03:04 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import Header from "../src/components/Header/Header";
+import About from "../src/components/About/About";
+import Contact from "../src/components/Contact/Contact";
+import Portfolio from "../src/components/Portfolio/Portfolio";
+import Resume from "../src/components/Resume/Resume";
+import Footer from "../src/components/Footer/Footer";
