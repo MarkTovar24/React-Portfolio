d0fd7e33a8ecb2dffc8493b104d934361848a6be
Mark Tovar <markjtovar24@gmail.com>
2024-05-09 22:13:17 -0500

    Added basic stuff in app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+//Import everything Needed
 import React, { useState } from "react";
 import Header from "../src/components/Header/Header";
 import About from "../src/components/About/About";
@@ -5,3 +6,37 @@ import Contact from "../src/components/Contact/Contact";
 import Portfolio from "../src/components/Portfolio/Portfolio";
 import Resume from "../src/components/Resume/Resume";
 import Footer from "../src/components/Footer/Footer";
+
+
+function App() {
+    const [currentComp, compChange] = useState("about");
+
+    if (currentComp === 'About') {
+        return <About />;
+    }
+    if (currentComp === 'Contact') {
+        return <Contact />
+    }
+    if (currentComp === 'Footer') {
+        return <Footer />
+    }
+    if (currentComp === 'Header') {
+        return <Header />
+    }
+    if (currentComp === 'Nav') {
+        return <Nav />
+    }
+    if (currentComp === 'Portfolio') {
+        return <Portfolio />
+    }
+    if (currentComp === 'Resume') {
+        return <Resume />
+    }
+
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
